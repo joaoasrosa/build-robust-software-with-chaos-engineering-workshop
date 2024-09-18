449c9a592e2b30e6d5493fbd1e4043a947517819
João Rosa <joaoasrosa@gmail.com>
2024-09-18 16:41:19 +0200

    Add retry policy

--- src/API/DataAccess/Routes.cs
+++ src/API/DataAccess/Routes.cs
@@ -1,27 +1,46 @@
 using System.Data;
 using Dapper;
+using Polly;
+using Polly.Retry;
 
 namespace API.DataAccess;
 
 public class Routes
 {
     private readonly IDbConnection _connection;
+    private readonly RetryPolicy _retryPolicy;
 
-    public Routes(IDbConnection connection)
+    public Routes(IDbConnection connection, IConfiguration configuration, ILogger<Routes> logger)
     {
         _connection = connection;
+
+        var retryCount = configuration.GetValue<int>("PollySettings:RetryCount");
+        var baseDelayMilliseconds = configuration.GetValue<int>("PollySettings:RetryBaseDelayMilliseconds");
+
+        _retryPolicy = Policy
+            .Handle<Exception>()
+            .WaitAndRetry(
+                retryCount: retryCount,
+                sleepDurationProvider: attempt =>
+                    TimeSpan.FromMilliseconds(baseDelayMilliseconds * Math.Pow(2, attempt - 1)),
+                onRetry: (exception, timeSpan, retryAttempt, context) =>
+                {
+                    logger.LogWarning(
+                        $"Retry {retryAttempt} after {timeSpan.TotalMilliseconds} ms due to: {exception.Message}");
+                }
+            );
     }
 
     public IEnumerable<Route> GetRoutes(string from, string to)
     {
-        return _connection.Query<Route>(
-            "SELECT source_airport.iata AS 'From', destination_airport.iata AS 'To', airlines.name AS 'Airline' "+
-            "FROM routes "+
-            "INNER JOIN airports source_airport on routes.src_apid = source_airport.apid "+
-            "INNER JOIN airports destination_airport on routes.dst_apid = destination_airport.apid "+
-            "INNER JOIN airlines on routes.alid = airlines.alid "+
+        return _retryPolicy.Execute(() => _connection.Query<Route>(
+            "SELECT source_airport.iata AS 'From', destination_airport.iata AS 'To', airlines.name AS 'Airline' " +
+            "FROM routes " +
+            "INNER JOIN airports source_airport on routes.src_apid = source_airport.apid " +
+            "INNER JOIN airports destination_airport on routes.dst_apid = destination_airport.apid " +
+            "INNER JOIN airlines on routes.alid = airlines.alid " +
             "WHERE source_airport.iata = @From AND destination_airport.iata = @To",
-            new {From = from, To = to}
-        ).ToList();
+            new { From = from, To = to }
+        ).ToList());
     }
 }
