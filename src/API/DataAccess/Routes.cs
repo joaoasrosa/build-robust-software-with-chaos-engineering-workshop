8bb76b13e01ec8da3b9dfa410bc037855a19db39
João Rosa <joaoasrosa@gmail.com>
2024-09-18 16:21:44 +0200

    Add timeout policy for the API

--- src/API/DataAccess/Routes.cs
+++ src/API/DataAccess/Routes.cs
@@ -1,3 +1,4 @@
+using System.Collections.Immutable;
 using System.Data;
 using Dapper;
 using Polly;
@@ -34,14 +35,14 @@ public class Routes
             );
     }
 
-    public IEnumerable<Route> GetRoutes(string from, string to)
+    public ImmutableArray<Route> GetRoutes(string from, string to)
     {
-        return _retryPolicy.Execute(() => _connection.Query<Route>(
-            "SELECT source_airport.iata AS 'From', destination_airport.iata AS 'To', airlines.name AS 'Airline' " +
-            "FROM routes " +
-            "INNER JOIN airports source_airport on routes.src_apid = source_airport.apid " +
-            "INNER JOIN airports destination_airport on routes.dst_apid = destination_airport.apid " +
-            "INNER JOIN airlines on routes.alid = airlines.alid " +
+        return _connection.Query<Route>(
+            "SELECT source_airport.iata AS 'From', destination_airport.iata AS 'To', airlines.name AS 'Airline' "+
+            "FROM routes "+
+            "INNER JOIN airports source_airport on routes.src_apid = source_airport.apid "+
+            "INNER JOIN airports destination_airport on routes.dst_apid = destination_airport.apid "+
+            "INNER JOIN airlines on routes.alid = airlines.alid "+
             "WHERE source_airport.iata = @From AND destination_airport.iata = @To",
             new { From = from, To = to }
         ).ToList());
