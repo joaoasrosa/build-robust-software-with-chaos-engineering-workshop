82be58b54792945bead6ca3954b75a4662ad30c3
João Rosa <joaoasrosa@gmail.com>
2024-09-19 09:52:33 +0200

    Add health endpoint, with MySql check

---
+++ src/API/CustomHealthCheckResponseWriter.cs
@@ -0,0 +1,33 @@
+using System.Text.Json;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace API;
+
+public static class CustomHealthCheckResponseWriter
+{
+    public static Task WriteResponse(HttpContext context, HealthReport report)
+    {
+        context.Response.ContentType = "application/json";
+
+        var response = new
+        {
+            status = report.Status.ToString(),
+            checks = report.Entries.Select(e => new
+            {
+                name = e.Key,
+                status = e.Value.Status.ToString(),
+                description = e.Value.Description ?? string.Empty,
+                duration = e.Value.Duration.TotalMilliseconds
+            }),
+            totalDuration = report.TotalDuration.TotalMilliseconds
+        };
+
+        return context.Response.WriteAsync(
+            JsonSerializer.Serialize(
+                response,
+                new JsonSerializerOptions
+                {
+                    WriteIndented = true
+                }));
+    }
+}
